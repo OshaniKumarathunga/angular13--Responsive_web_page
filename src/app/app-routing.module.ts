6180edb290e12f62e25df3e210b540bbd0368f16
OshaniKumarathunga <oshanipbkonline@gmail.com>
2023-01-21 23:33:09 +0530

    doctor page created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// importing routing components
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { DoctorComponent } from './doctor/doctor.component';
+
+// routes for the components
+const routes: Routes = [
+  // {path: 'home', component:},
+  {path: 'doctor' , component:DoctorComponent}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// import flexLayout
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+import 'hammerjs';
+
+// Angular material
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { DoctorComponent } from './doctor/doctor.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    DoctorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
