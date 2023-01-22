cbcb659cd318e306549c4741787c8bdea4003449
OshaniKumarathunga <oshanipbkonline@gmail.com>
2023-01-23 00:38:44 +0530

    doctor sign up coded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 // importing routing components
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { DoctorComponent } from './doctor/doctor.component';
+import { DoctorSignupComponent } from './doctor-signup/doctor-signup.component';
 
 // routes for the components
 const routes: Routes = [
   // {path: 'home', component:},
-  {path: 'doctor' , component:DoctorComponent}
+  {path: 'doctor' , component:DoctorComponent},
+  {path:'signup' ,component:DoctorSignupComponent }
 
 ];
 
